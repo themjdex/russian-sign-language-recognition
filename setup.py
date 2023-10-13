9298d40b8d49627a96f20a0793e2a370dda23786
Marat Zhanpeisov <themjdex@gmail.com>
2023-10-13 14:40:11 +0500

    add baseline project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Russian sign language recognition',
+    author='Marat Zhanpeisov',
+    license='MIT',
+)
